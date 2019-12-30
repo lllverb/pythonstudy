e8824af587fe597de9712bc0b5b269f7740b0020
lllverb <54735495+lllverb@users.noreply.github.com>
2019-12-31 02:30:17 +0900

    tensorflow installed

--- rename.py
+++ rename.py
@@ -1,8 +1,8 @@
-import os
-import glob
+# import os
+# import glob
 
-path = "./cutted/yoshizawa_cutted"
-files = glob.glob(path + "/*")
+# path = "./cutted/yoshizawa_cutted"
+# files = glob.glob(path + "/*")
 
-for i, f in enumerate(files, 1):
-    os.rename(f, os.path.join(path, "yoshizawa_cutted_" + str(i) + ".jpg"))
+# for i, f in enumerate(files, 1):
+#     os.rename(f, os.path.join(path, "yoshizawa_cutted_" + str(i) + ".jpg"))
