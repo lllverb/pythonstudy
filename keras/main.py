735ff365ee79927a8729817cde2b3607cb16018e
lllverb <54735495+lllverb@users.noreply.github.com>
2020-01-04 00:42:37 +0900

    keras

---
+++ keras/main.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# import sys
+import os
+
+# import numpy as np
+# import pandas as pd
+# from PIL import Image
+from model import MyModel
+
+images_dir = "./train_all"
+inputs_dir = "./test/hashimoto"
+categories = [name for name in os.listdir(images_dir) if name != ".DS_Store"]
+inputs = [name for name in os.listdir(inputs_dir) if name != ".DS_Store"]
+
+# 入力画像の予測値
+predictions = MyModel().predict_from_dir(inputs_dir)
+
+# 結果出力
+strcat = ""
+for cat in categories:
+    strcat += cat.ljust(11, " ")
+print("                       ", strcat)
+
+for i, input in enumerate(inputs):
+    strpre = input.ljust(28, " ")
+    for pre in predictions[i]:
+        strpre += str(round(pre, 3)).ljust(10, " ")
+    print(strpre)
